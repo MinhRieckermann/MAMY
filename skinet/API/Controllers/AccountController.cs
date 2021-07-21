905c5cfb74f22e54cb68da5190f71caddeee6665
bsportteam <bsportteamminh@outlook.com>
2021-07-21 23:57:47 +0700

    End of Validation API

--- skinet/API/Controllers/AccountController.cs
+++ skinet/API/Controllers/AccountController.cs
@@ -97,8 +97,12 @@ namespace API.Controllers
 
     [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
-    {
-        var user = new AppUser
+    {   
+        if (CheckEmailExistAsync(registerDto.Email).Result.Value)
+        {
+            return new BadRequestObjectResult(new ApiValidationErrorResponse{Error= new [] {"Email address is in use"}});
+        }
+        var user =  new AppUser
         {
             DisplayName = registerDto.DisplayName,
             Email = registerDto.Email,
