155f44efc6fc13952ebf50d3c39c482c0e9eb932
bsportteam <bsportteamminh@outlook.com>
2021-08-20 01:45:16 +0700

    end of dev identity

--- skinet/client/src/app/app.component.ts
+++ skinet/client/src/app/app.component.ts
@@ -41,7 +41,7 @@ export class AppComponent implements OnInit {
   loadCurrentUser(){
     const token =localStorage.getItem('token');
 
-    if (token){
+    
       this.accountService.loadCurrentUser(token).subscribe(()=>{
         console.log('loaded user')
       },error =>{
@@ -50,7 +50,7 @@ export class AppComponent implements OnInit {
 
 
       )
-    }
+    
   }
   
 }
